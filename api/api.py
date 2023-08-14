1b74223d53be1742d63603c535b87acccfb666fa
Marceau-h <106751184+Marceau-h@users.noreply.github.com>
2023-08-14 19:09:55 +0200

    WIP : code cleaning + zip as a File child class

--- api/api.py
+++ api/api.py
@@ -3,8 +3,7 @@ import os
 from typing import List
 from pathlib import Path
 
-
-from fastapi import FastAPI, UploadFile, Form, File  # , Query
+from fastapi import FastAPI, UploadFile, Form, File
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import StreamingResponse, HTMLResponse, FileResponse
 
@@ -12,19 +11,15 @@ from transformers import utils, pipeline
 from transformers.enums import MimeType, Output, Tag
 
 main_dir = Path(__file__).parent
-root_dir = main_dir.parent
 
-host = os.getenv("TXM_SERVER", "")
 app = FastAPI()
-
-templates = main_dir / "templates"
 
 app.mount("/static", StaticFiles(directory=main_dir / "static"), name="static")
 
 
 @app.get("/", response_class=HTMLResponse, tags=["main"])
 async def read_root():
-    with open(templates / "index.html") as f:
+    with open(main_dir / "templates/index.html") as f:
         return f.read()
 
 
@@ -34,33 +29,23 @@ async def write_upload(
 ):
     files = [utils.File(f.filename, await f.read()) for f in files]
 
-    # result_names: List[str]
-    # result: List[str | bytes]
-    # result_mime: List[MimeType]
+    outputs: dict[Output, list[Tag]]
+    output_types: list[Output]
+
     results: List[File]
     result: File
-
-    # print(f"{output = }")
-    # print(f"{files = }")
+    res: io.StringIO | io.BytesIO
+    mime: MimeType
+    name: str
 
     outputs = {Output(o): [Tag("text")] for o in output if not "-" in o}
     for o in output:
         o = o.split("-")
         if len(o) == 2:
-            outputs[o[0]].append(Tag(o[1]))
+            outputs[Output(o[0])].append(Tag(o[1]))
 
     output_types = [k for k in outputs]
     output_tags = [outputs[k] for k in outputs]
-
-    # print(f"{outputs = }")
-    # print(len(outputs))
-    # print(f"{output_types = }")
-    # print(f"{output_tags = }")
-    #
-    # res = pipeline.pipeline(files, output_types, output_tags)
-
-
-    # result_names, result, result_mime = pipeline.pipeline(files, output_types, output_tags).values()
 
     results = pipeline.pipeline(files, output_types, output_tags)
 
@@ -72,44 +57,22 @@ async def write_upload(
         else:
             res = io.BytesIO(result.content)
 
-        return StreamingResponse(
-            content=res,
-            status_code=200,
-            media_type=result.mime_type.value,
-            headers={"Content-Disposition": f"attachment; filename={result.name}"},
-        )
+        mime = result.mime_type
+        name = result.name
 
+    else:
+        zip = utils.ZipCreator("results.zip", mode="names")
+        zip.fill_zip(results)
 
-    # zip_io = io.BytesIO()
-    # with zipfile.ZipFile(zip_io, mode="w", compression=zipfile.ZIP_DEFLATED) as temp_zip:
-    #     for result in results:
-    #         print(f"{result.name = }; {result.mime_type = }")
-    #         if result.mime_type == MimeType.zip:
-    #             temp_zip.mkdir(result.name)
-    #             with zipfile.ZipFile(io.BytesIO(result.content), mode="r") as z:
-    #                 for f in z.namelist():
-    #                     temp_zip.writestr(f"{result.name}/{f}", z.read(f))
-    #             continue
-    #
-    #         temp_zip.writestr(f"{result.name}", result.content)
-    #
-    # zip_io.seek(0)
+        res = zip.content
 
-    zipfile = utils.ZipCreator(results)
+        mime = MimeType.zip
 
+        name = "results.zip"
 
     return StreamingResponse(
-        content=zipfile,
+        content=res,
         status_code=200,
-        media_type=MimeType.zip.value,
-        headers={"Content-Disposition": "attachment; filename=results.zip"},
+        media_type=mime.value,
+        headers={"Content-Disposition": f"attachment; filename={name}"},
     )
-
-
-
-
-
-
-
-
-
