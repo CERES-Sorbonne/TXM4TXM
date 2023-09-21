e8d67f9b4b964a32b06027f6beb8bcd58ca11c19
Marceau-h <106751184+Marceau-h@users.noreply.github.com>
2023-09-21 13:41:21 +0200

    Env variable to set a host

--- api/api.py
+++ api/api.py
@@ -17,8 +17,8 @@ main_dir = Path(__file__).parent
 
 app = FastAPI()
 
-host = os.getenv("TXM4TXM_SERVER", "")
-prefix = os.getenv("TXM4TXM_PREFIX", "")
+host = os.getenv("TXM_PATH", "")
+prefix = os.getenv("TXM_PREFIX", "")
 
 app.mount("/static", StaticFiles(directory=main_dir / "static"), name="static")
 templates = Jinja2Templates(directory=os.path.join(main_dir, "templates"))
